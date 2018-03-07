cce8ace751197caf82a08f1384f3ac6569c4db9a
Anthony Ochiche <Anthony.Ochiche@polkcountyiowa.gov>
2018-03-07 10:17:24 -0600

    Initial Commit...

---
+++ Person.DomainClasses/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person.DomainClasses
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string CompanyName { get; set; }
+        public string ContactTitle { get; set; }
+        public int PersonId { get; set; }
+        public Person Person { get; set; }
+
+
+    }
+}
---
+++ Person.DomainClasses/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person.DomainClasses
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Title { get; set; }
+        public DateTime? HireDate { get; set; }
+        public int PersonId { get; set; }
+        public Person Person { get; set; } 
+    }
+}
